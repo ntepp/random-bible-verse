141110dabfb222b02b9bbcb4272c41d4d6be1f72
ntepp <ntepp.marcus@gmail.com>
2023-11-22 06:10:22 +0100

    add kubernates deployment and service yml

--- src/main/java/com/perinfinity/com/randomverse/RandomVersesApplication.java
+++ src/main/java/com/perinfinity/com/randomverse/RandomVersesApplication.java
@@ -1,7 +1,5 @@
 package com.perinfinity.com.randomverse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -10,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RandomVersesApplication {
-	private static final Logger log = LoggerFactory.getLogger(RandomVersesApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandomVersesApplication.class, args);
--- src/main/java/com/perinfinity/com/randomverse/controller/VerseController.java
+++ src/main/java/com/perinfinity/com/randomverse/controller/VerseController.java
@@ -1,6 +1,9 @@
 package com.perinfinity.com.randomverse.controller;
 
+import com.perinfinity.com.randomverse.RandomVersesApplication;
 import com.perinfinity.com.randomverse.domain.Verse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +16,12 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class VerseController {
 
+    private int request = 1;
+    private static final Logger log = LoggerFactory.getLogger(VerseController.class);
     @Autowired RestTemplate restTemplate;
     @GetMapping("/")
     public String getVerse(Model model) {
+        log.info("{} request to Bible verse ", request++);
         Verse verse = restTemplate.getForObject(
                 "https://bible-api.com/?random=verse", Verse.class);
         model.addAttribute("reference", verse.reference());
